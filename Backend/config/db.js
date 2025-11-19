ad616b721e65c02291c7610614cc7d7f2bee9ed3
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2025-11-19 20:32:15 +0600

    db connection done

---
+++ Backend/config/db.js
@@ -0,0 +1,12 @@
+// supabase.js
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+export default db;
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
+// index.js
+import express from "express";
+import db from "./config/db.js";
+
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
+app.get("/test", async (req, res) => {
+    // basic test query
+    const { data, error } = await db
+        .from("users")
+        .select("*");
+
+    if (error) return res.status(400).json({ error });
+
+    res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+app.listen(3000, () => console.log("Server running on port 5000"));
